ca234531ae0ce325f80c7ca199d1b438abc100eb
Shivam Patsariya <s.patsariya@gmail.com>
2025-10-07 02:02:09 -0400

    fd

--- src/Services/BaseUrl.js
+++ src/Services/BaseUrl.js
@@ -1,8 +1,8 @@
-
-const baseUrl = 'https://lab2.invoidea.in/outlier';
+const baseUrl = 'https://admin.droutlier.com';
+// const baseUrl = 'https://lab2.invoidea.in/outlier';
 
 // const baseUrl = 'https://droutlier.com/app';
-// const baseUrl = 'https://admin.droutlier.com';
+
 
 
 module.exports = baseUrl;   
