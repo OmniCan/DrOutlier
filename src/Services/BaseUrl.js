716175e28bf9a2f55f1748750fb0567240b21891
Shivam Patsariya <s.patsariya@gmail.com>
2025-10-07 00:43:39 -0400

    URL Update

--- src/Services/BaseUrl.js
+++ src/Services/BaseUrl.js
@@ -2,7 +2,7 @@
 // const baseUrl = 'https://lab2.invoidea.in/outlier';
 
 // const baseUrl = 'https://droutlier.com/app';
-const baseUrl = 'https://app.droutlier.com';
+const baseUrl = 'https://admin.droutlier.com';
 
 
 module.exports = baseUrl;   
