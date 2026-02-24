30c066a201aacbbc8cec82da4fb3c8efe00e7d5c
spatsariya <s.patsariya@gmail.com>
2026-02-23 21:54:04 -0500

    I've added a 40px top margin to the container holding the question block. This creates a nice breathing space between the gradient header (with the pagination and "Go Back" button) and the white question card below it.
    The spacing will make the layout look cleaner and more professional. You can adjust the value (marginTop: '40px') to your preference if you want more or less space!

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -319,9 +319,8 @@ const Page = () => {
                     </div>
                   </div>
                 </div>
-                                      <div className="col-lg-4" style={{ lineHeight: '24px' }}>
 
-                <div className="container">
+                <div className="container" style={{ marginTop: '40px' }}>
                   <div
                     className="macaroni-sign-wrap p-4"
                     style={{ backgroundColor: "#fff" }}
